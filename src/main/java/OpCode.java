244cf9b2821c2a53d9addc2a1fa0c5fd7abdb67b
Bharath Talluri <bharath3794@gmail.com>
2024-07-09 05:16:33 -0500

    parse header section fixing OpCode

--- src/main/java/OpCode.java
+++ src/main/java/OpCode.java
@@ -6,7 +6,20 @@ import java.util.stream.Stream;
 public enum OpCode {
     QUERY(0),
     IQUERY(1),
-    STATUS(2);
+    STATUS(2),
+    RESERVED1(3),
+    RESERVED2(4),
+    RESERVED3(5),
+    RESERVED4(6),
+    RESERVED5(7),
+    RESERVED6(8),
+    RESERVED7(9),
+    RESERVED8(10),
+    RESERVED9(11),
+    RESERVED10(12),
+    RESERVED11(13),
+    RESERVED12(14),
+    RESERVED13(15);
 
     private int code;
 
