bc57bb04bddae47454b58a7806354727d7a4dc67
Bharath Talluri <bharath3794@gmail.com>
2024-07-11 10:16:54 -0500

    test

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -176,6 +176,6 @@ public class Answer {
                 .putShort(this.getAnClass())
                 .putInt(this.getAnTtl())
                 .putShort(this.getAnRLength())
-                .put(Answer.encodedAnRData(this.getAnRData()));
+                .put(this.getAnRData());
     }
 }
