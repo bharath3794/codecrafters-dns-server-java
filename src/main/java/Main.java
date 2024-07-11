fc75076657cc33c92a89519005ac67cfa75ac009
Bharath Talluri <bharath3794@gmail.com>
2024-07-11 05:54:48 -0500

    forwarding server read arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
 public class Main {
+  public static String resolver = null;
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    try (DnsServer dnsServer = new DnsServer(2053)){
+    if (args.length > 1 && args[0].equals("--resolver")) {
+      resolver = args[1];
+    }
+
+    try (
+              DnsServer dnsServer = Main.resolver == null ? new DnsServer(2053) : new DnsServer(2053, Main.resolver);
+    ) {
       dnsServer.start();
     }
   }
